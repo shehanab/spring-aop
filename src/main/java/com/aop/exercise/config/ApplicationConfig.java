9d11207ec40b48cf2c40724ddcb664010f885b3b
p0103317 <shehan.a@ncs.com.sg>
2020-08-22 08:11:07 +0800

    AOP exercise project config files

---
+++ src/main/java/com/aop/exercise/config/ApplicationConfig.java
@@ -0,0 +1,15 @@
+package com.aop.exercise.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.aop.exercise")
+@EnableAspectJAutoProxy
+public class ApplicationConfig {
+
+
+}
